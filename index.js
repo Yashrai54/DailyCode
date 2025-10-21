5a207dfe566ea68c16eb17f3af36853f8b2e27bf
Yash Rai <157264996+Yashrai54@users.noreply.github.com>
2025-10-21 14:33:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-iimport express from 'express';
+import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import userRouter from './routes/user.routes.js';
