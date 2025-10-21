3f12000938a185a6733e0e67fb7663d6760b8b55
Yash Rai <157264996+Yashrai54@users.noreply.github.com>
2025-10-21 13:16:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ import { createDailyContest } from './controllers/contest.controller.js';
 import contestRouter from './routes/contest.routes.js';
 import submissionRouter from './routes/submission.routes.js';
 import { error } from 'console';
-import Contest from './models/contest.model.js';
 import isAuth from './middlewares/isAuth.js';
 dotenv.config();
 
@@ -27,7 +26,6 @@ app.set("view engine","ejs");
 app.use('/api/users',userRouter);
 app.use("/api/contest",contestRouter)
 app.use("/api/submission",submissionRouter)
-    const result=await Contest.find().sort({createdAt:-1}).limit(1);
 
 app.get("/signup",(req,res)=>{
     res.render("signup",{error:null});
